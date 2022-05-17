f8f8c61da1e25b046cc905b2b475da5a28fe0fb8
Rachel Nascimento <rachelle.nascimento@gmail.com>
2022-05-17 11:25:38 +0200

    add gem faker and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Creating 10 articles..."
+10.times do |i|
+  article = Article.create!(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraph
+  )
+  puts "#{i + 1}. #{article.title}"
+end
+puts "Finished!"
